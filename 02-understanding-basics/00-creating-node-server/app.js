b13ca22944bc5d3110bef155c9e5c261f2279503
abhinav-88 <123355323+abhinav-88@users.noreply.github.com>
2023-07-21 11:27:30 +0530

    Section 4: Started.Improved Development Workflow & Debugging

--- 02-understanding-basics/00-creating-node-server/app.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const server  = http.createServer((req,res) => {
-    // This arrow function will now run for every request that reaches our server
-    console.log(req);
-    // process.exit();
-});
-server.listen(3000);
