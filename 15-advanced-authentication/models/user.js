fdaefbe7b3bc80c3624b7421fb50ceeebf898ccd
abhinav-88 <123355323+abhinav-88@users.noreply.github.com>
2023-08-02 12:39:45 +0530

    Error identification from Instructor

--- 15-advanced-authentication/models/user.js
+++ 15-advanced-authentication/models/user.js
@@ -11,6 +11,8 @@ const userSchema = new Schema({
     type: String,
     required: true
   },
+  resetToken: String,
+  resetTokenExpiration: Date,
   cart: {
     items: [
       {
@@ -191,3 +193,5 @@ module.exports = mongoose.model('User', userSchema);
 // }
 
 // module.exports = User;
+
+
